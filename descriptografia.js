a56f04ba78b177930837e81a0a5f4529eb911d36
Andrea <andrealh08@gmail.com>
2022-05-23 11:41:13 -0300

    feat: cria a funcao descriptografar

---
+++ descriptografia.js
@@ -0,0 +1,19 @@
+const crip = 'htinltB678*';
+
+const descriptografar = (string) => {
+  const toCesar = string.split('').map((char) => {
+    if ((char >= 'a' && char <= 'c') || (char >= 'A' && char <= 'C')) {
+      return String.fromCharCode(char.charCodeAt(0) + 23);
+    }
+    return String.fromCharCode(char.charCodeAt(0) - 3);
+  });
+
+  const toAscii = toCesar.map((char) => {
+    return String.fromCharCode(char.toString().charCodeAt(0) - 2);
+  }); 
+  return toAscii.join('');
+};
+
+const codigo = descriptografar(crip);
+
+console.log(`Seu codigo: ${codigo}`);
