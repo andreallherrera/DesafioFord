173e8b9f1c9d75183eca743fab9bcdbcc0577061
Andrea <andrealh08@gmail.com>
2022-05-23 11:38:41 -0300

    feat: cria a funcao criptografar

---
+++ criptografia.js
@@ -0,0 +1,19 @@
+const codigo = 'codigoW123%';
+
+const criptografar = (string) => {
+  const firstToAscii = string.split('').map((char) => {
+    return String.fromCharCode(char.charCodeAt(0) + 2);
+  });
+  
+  const toCesar = firstToAscii.map((char) => {
+    if ((char >= 'x' && char <= 'z') || (char >= 'X' && char <= 'Z')) {
+      return String.fromCharCode(char.charCodeAt(0) - 23);
+    }
+    return String.fromCharCode(char.charCodeAt(0) + 3);
+  }); 
+  return toCesar.join('');
+};
+
+const crip = criptografar(codigo);
+
+console.log(`Seu codigo: ${crip}`);
